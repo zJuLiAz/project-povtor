905efa7b761957d5843164a6ddc4a4704469587f
Julia <zz-julia-zz@mail.ru>
2021-03-19 15:01:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 === 8);
+    
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
